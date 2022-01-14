eaac6ffad779251ffc3c3c497681ec6e8ebc5a22
t4traw <t4traw@gmail.com>
2022-01-14 21:52:34 +0900

    使っていないuseStoreを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import Menu from './components/Menu'
 import menuList from './menuList'
-import { useStore } from './store'
 
 const Yakitori = () => {
-  const state = useStore()
-
   return (
     <div className="pb-24">
       <div className="grid grid-cols-1 gap-2">
